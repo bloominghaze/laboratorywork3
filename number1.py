e4a905e3713c9db602b1494674363be252913524
bloominghaze <twix8d@gmail.com>
2024-10-04 11:59:11 +0300

    123

--- number1.py
+++ number1.py
@@ -1,20 +1,7 @@
-s = input("Введіть довільний рядок: ")
+t = str(input("Введіть слово: "))
 
-letters_only = [char for char in s if char.isalpha()]
+half_length = len(t) // 2
 
-half_length = len(letters_only) // 2
+result = t[:half_length]
 
-result = ''
-letter_count = 0
-
-for char in s:
-    if char.isalpha():
-        if letter_count < half_length:
-            result += char
-            letter_count += 1
-        else:
-            break
-    else:
-        result += char
-
-print(f"Половина рядка: {result}")
+print("Половина літер слова: ", result)
